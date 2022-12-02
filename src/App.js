650e2ed3666bb3ad34df601bcbee0a49b9370dd0
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2022-12-02 12:42:21 +0530

    app dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
+    </div>
+  );
+}
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Dashboard😍</h1>
+      <SummaryBox />
+      {/* <SummaryBox /> */}
     </div>
   );
 }
 
+function SummaryBox() {
+  const data = {
+    text: "Number of sales",
+    number: 568,
+    percent: 0.7,
+    performance: "up",
+    time: "Last Month",
+    type: "number",
+  };
+  return (
+    <div className="summary-box-container">
+      <p className="summary-box-text">{data.text}</p>
+      <h2 className="summary-box-number">
+        {(data.type = "money" ? "$" : null)}
+        {data.number}
+      </h2>
+      <div className="summary-box-time-container">
+        <p>{data.time}</p>
+        <p>{data.percent}%</p>
+      </div>
+    </div>
+  );
+}
 export default App;
